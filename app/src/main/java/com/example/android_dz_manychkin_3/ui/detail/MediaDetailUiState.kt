4a7880bbf30c233824090a550c7fc68bc9e79d50
amoui <artimediw37916002@gmail.com>
2026-05-09 16:57:19 +1000

    Revert "Merge pull request #4 from ammoui/hw3"
    This reverts commit 3899e4605a811d163216a483e02ad4cf167a3b7a, reversing
    changes made to 039f25ff153c170913a73f17b666586cb8cb1f26.

--- app/src/main/java/com/example/android_dz_manychkin_3/ui/detail/MediaDetailUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android_dz_manychkin_3.ui.detail
-
-import com.example.android_dz_manychkin_3.model.MediaDetail
-
-data class MediaDetailUiState(
-    val detail: MediaDetail? = null,
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null,
-)
